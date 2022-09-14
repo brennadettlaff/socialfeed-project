16d9aa4843383c13b80156fa66d4ac06da29d8ab
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-14 15:10:28 -0600

    added functionality to add post component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ function App() {
 
   const [posts, setPosts] = useState([{name: 'name', body: 'posts will display here'}, {name: 'name 2', body:'body 2'}])
 
+  function addNewPost(post){
+    let tempPosts = [...posts, post];
+    setPosts(tempPosts);
+  }
+
   return (
     <div>
       <DisplayPosts parentEntries={posts} />
-      <AddPost />
+      <AddPost addNewPost={addNewPost}/>
 
     </div>
   );
--- src/Components/AddPost/AddPost.jsx
+++ src/Components/AddPost/AddPost.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
 
 const AddPost = (props) => {
+    const [name, setName] = useState('');
+    const [body, setBody] = useState('');
+
+    function handleSubmit(event){
+        event.preventDefault();
+        let newPost = {
+            name: name,
+            body: body
+        };
+        console.log(newPost)
+        props.addNewPost(newPost)
+    }
 
     return ( 
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Name</label>
-                <input type='text'></input>
+                <input type='text' value={name} onChange={(event) => setName(event.target.value)}></input>
             </div>
             <div>
                 <label>Post</label>
-                <input type='text'></input>
+                <input type='text' value={body} onChange={(event) => setBody(event.target.value)}></input>
             </div>
-
+            <button type='submit'>Create</button>
         </form>
      );
 }
